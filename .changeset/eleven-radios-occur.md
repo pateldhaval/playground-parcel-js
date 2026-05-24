bfbd3f298cf682685ec5cf2b38108192c22d0b19
Dhaval Patel <pateldhaval3890@gmail.com>
2026-05-24 15:01:18 +0530

    patch change to test multiple changeset

---
+++ .changeset/eleven-radios-occur.md
@@ -0,0 +1,5 @@
+---
+'parcel-vanilla-starter': patch
+---
+
+setupCounter step updated with 10
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,6 @@ import { setupCounter } from './utils/counter';
 import { greet } from './utils/global';
 
 const button = document.getElementById('counter');
-setupCounter(button as HTMLElement, 5);
+setupCounter(button as HTMLElement, 10);
 
 console.log(greet('John'));
